be1d084fe26365307cee998c326630fb60d4141a
Nataliya Pogrebniak <pogrebnyak.nataliya@gmail.com>
2022-09-28 23:06:44 +0300

    add files

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { ButtonList, Option, Button } from './FeedbackOptions.styled';
+import { btnBgColorFun } from './btnBgColor';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ButtonList>
+      {options.map(option => {
+        return (
+          <Option key={option}>
+            <Button
+              style={{ backgroundColor: btnBgColorFun(option) }}
+              type="button"
+              name={option}
+              onClick={onLeaveFeedback}
+            >
+              {option}
+            </Button>
+          </Option>
+        );
+      })}
+    </ButtonList>
+  );
+};
+
+FeedbackOptions.prototype = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+  btnBgColorFun: PropTypes.func.isRequired,
+};
